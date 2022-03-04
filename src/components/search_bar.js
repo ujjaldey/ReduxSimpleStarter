47256ba526d0f355a9f91fa969ec19464919f7d3
Ujjal Dey <ujjaldey@gmail.com>
2022-03-04 10:00:05 +0800

    #249

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,17 @@
-import React from "react";
+// {} means, import React and pull of the Component as a variable Component
+// i.e. const Component = React.Component
+import React, { Component } from "react";
 
-const SearchBar = () => {
-    return <input />;
-};
+// functional component
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+// class based component
+class SearchBar extends Component {
+    render() {
+        return <input onChange={event => console.log(event.target.value)} />;
+    }
+}
 
 export default SearchBar;
