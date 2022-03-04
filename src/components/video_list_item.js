d00e61c3ca812c79cc843b153d5dfa158228cd69
Ujjal Dey <ujjaldey@gmail.com>
2022-03-04 10:58:57 +0800

    #259

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+// instead of (props), and then const video = props.video, we can use ({video})
+const VideoListItem = ({ video }) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    console.log(imageUrl);
+
     return (
-        <li>Video</li>
+        <li className='list-group-item'>
+            <div className='video-list media'>
+                <div className='media-left'>
+                    <img className='media-object' src={imageUrl} />
+                </div>
+                <div className='media-body'>
+                    <div className='media-heading'>
+                        {video.snippet.title}
+                    </div>
+                </div>
+            </div>
+        </li>
     );
 }
 
