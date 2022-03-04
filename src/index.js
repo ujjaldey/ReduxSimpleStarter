476562b3d4254149a0c7f6ae20047af035f01910
Ujjal Dey <ujjaldey@gmail.com>
2022-03-04 10:17:50 +0800

    #254

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 // get the api key from google api console:
 const API_KEY = 'AIzaSyBXVDKNM48G5N5WYkBMyfplOT8FM5uorU8';
+
+YTSearch({
+    key: API_KEY, term: 'surfboards', function(data) {
+        console.log(data);
+    }
+});
 
 // create a new component. this component should produce some html
 const App = () => { // ES6 syntax
