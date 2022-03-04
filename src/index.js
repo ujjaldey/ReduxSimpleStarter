cf6e5cc330d7f5e13df474acb8d0e6598b5d5530
Ujjal Dey <ujjaldey@gmail.com>
2022-03-04 09:49:01 +0800

    #247

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 // get the api key from google api console:
 const API_KEY = 'AIzaSyBXVDKNM48G5N5WYkBMyfplOT8FM5uorU8';
@@ -7,7 +8,11 @@ const API_KEY = 'AIzaSyBXVDKNM48G5N5WYkBMyfplOT8FM5uorU8';
 // create a new component. this component should produce some html
 const App = () => { // ES6 syntax
     // this is jsx: <div>Hi</div>, which generates the html when transpiled
-    return <div>Hi</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // take this component's generated html and put it on the page (in the dom)
