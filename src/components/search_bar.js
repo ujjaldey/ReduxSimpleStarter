ddb715df9f9c3205afcb5186110dc1d3ae44cefe
Ujjal Dey <ujjaldey@gmail.com>
2022-03-04 10:03:07 +0800

    #250

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,6 +9,11 @@ import React, { Component } from "react";
 
 // class based component
 class SearchBar extends Component {
+    constructor(props) {
+        super(props); // calls the constructor of the parent extends class
+        this.state = { term: '' };
+    }
+
     render() {
         return <input onChange={event => console.log(event.target.value)} />;
     }
