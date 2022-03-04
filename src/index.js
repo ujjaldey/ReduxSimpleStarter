9bddcb2247594e162eabddd9e8113f72e55b152b
Ujjal Dey <ujjaldey@gmail.com>
2022-03-04 09:44:17 +0800

    #246

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// get the api key from google api console:
+const API_KEY = 'AIzaSyBXVDKNM48G5N5WYkBMyfplOT8FM5uorU8';
+
 // create a new component. this component should produce some html
 const App = () => { // ES6 syntax
     // this is jsx: <div>Hi</div>, which generates the html when transpiled
